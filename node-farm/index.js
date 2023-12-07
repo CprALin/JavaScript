99150c34f3306f364b22af6064ccb532005953bc
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2023-12-07 16:58:02 +0200

    finish

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const replaceTamplate = require('./modules/replaceTemplate');
 
 //FILES
 //blocking , synchronous way
@@ -41,25 +42,6 @@ const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.h
 const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
-const replaceTamplate = (temp , product) =>
-{
-    let output = temp.replace(/{%PRODUCTNAME%}/g , product.productName);
-    output = output.replace(/{%IMAGE%}/g , product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g , product.from);
-    output = output.replace(/%NUTRIENTS%/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g , product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g , product.id);
-
-    if(!product.organic)
-    {
-        output = output.replace(/{%NOT_ORGANIC%}/g , 'not-organic');
-    }
-
-    return output;
-}
 
 const server = http.createServer((req , res) => {
     //console.log(req.url);
